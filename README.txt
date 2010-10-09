5999d3c5c6e6343d4953f317143802ec4b0421ad
Shane Graber <sgraber@gmail.com>
2010-10-08 22:04:38 -0400

    initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+Introduction
+============
+
+aaolm.typogrify is a simple Product for Plone that implements smartypants.py
+and typogrify.py to style Plone content.
+
+The current committed version works with Plone 4.0.  However it is _very_ slow.
---
+++ aaolm/typogrify/transform.py
@@ -0,0 +1,27 @@
+﻿import re
+
+from zope.interface import implements, Interface
+from zope.component import adapts
+
+from aaolm.typogrify.typogrify import typogrify
+
+from plone.transformchain.interfaces import ITransform
+
+class TypogrifyTransform(object):
+    implements(ITransform)
+    adapts(Interface, Interface) # any context, any request
+    
+    order = 1000
+    
+    def __init__(self, published, request):
+        self.published = published
+        self.request = request
+    
+    def transformBytes(self, result, encoding):
+        return typogrify(result)
+        
+    def transformUnicode(self, result, encoding):
+        return typogrify(result)
+    
+    def transformIterable(self, result, encoding):
+        return [typogrify(r) for r in result]
